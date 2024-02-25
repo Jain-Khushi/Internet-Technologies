0c4a2b2666aa8dd65efd7a9c9e2efb34f53abe59
Jain-Khushi <161002343+Jain-Khushi@users.noreply.github.com>
2024-02-25 13:50:41 +0530

    Add files via upload

---
+++ tables.js
@@ -0,0 +1,91 @@
+setTimeout(function(){
+    var text="";
+    document.getElementById('t2').innerHTML="<h4>Table of 2</h4>";
+    for (let i=1;i<=10;i++){
+        let n =i*2;
+        text="2 X "+i+" = "+n+"<br>"
+        document.getElementById('t2').innerHTML+=text;
+        }
+    document.getElementById('t2').innerHTML+="<br>";
+},2000);
+
+setTimeout(function table3(){
+    var text="";
+    document.getElementById('t3').innerHTML="<h4>Table of 3</h4>";
+    for (let i=1;i<=10;i++){
+        let n =i*3;
+        text="3 X "+i+" = "+n+"<br>";
+        document.getElementById('t3').innerHTML+=text;
+    }
+    document.getElementById('t3').innerHTML+="<br>";
+},4000);
+
+setTimeout(function table4(){
+    var text="";
+    document.getElementById('t4').innerHTML="<h4>Table of 4</h4>";
+    for (let i=1;i<=10;i++){
+        let n =i*4;
+        text="4 X "+i+" = "+n+"<br>";
+        document.getElementById('t4').innerHTML+=text;
+    }
+},6000);
+
+setTimeout(function table5(){
+    var text="";
+    document.getElementById('t5').innerHTML="<h4>Table of 5</h4>";
+    for (let i=1;i<=10;i++){
+        let n =i*5;
+        text="5 X "+i+" = "+n+"<br>";
+        document.getElementById('t5').innerHTML+=text;
+    }
+},8000);
+
+setTimeout(function table6(){
+    var text="";
+    document.getElementById('t6').innerHTML="<h4>Table of 6</h4>";
+    for (let i=1;i<=10;i++){
+        let n =i*6;
+        text="6 X "+i+" = "+n+"<br>";
+        document.getElementById('t6').innerHTML+=text;
+    }
+},10000);
+
+setTimeout(function table7(){
+    var text="";
+    document.getElementById('t7').innerHTML="<h4>Table of 7</h4>";
+    for (let i=1;i<=10;i++){
+        let n =i*7;
+        text="7 X "+i+" = "+n+"<br>";
+        document.getElementById('t7').innerHTML+=text;
+    }
+},12000);
+
+setTimeout(function table8(){
+    var text="";
+    document.getElementById('t8').innerHTML="<h4>Table of 8</h4>";
+    for (let i=1;i<=10;i++){
+        let n =i*8;
+        text="8 X "+i+" = "+n+"<br>";
+        document.getElementById('t8').innerHTML+=text;
+    }
+},14000);
+
+setTimeout(function table9(){
+    var text="";
+    document.getElementById('t9').innerHTML="<h4>Table of 9</h4>";
+    for (let i=1;i<=10;i++){
+        let n =i*9;
+        text="9 X "+i+" = "+n+"<br>";
+        document.getElementById('t9').innerHTML+=text;
+    }
+},16000);
+
+setTimeout(function table10(){
+    var text="";
+    document.getElementById('t10').innerHTML="<h4>Table of 10</h4>";
+    for (let i=1;i<=10;i++){
+        let n =i*10;
+        text="10 X "+i+" = "+n+"<br>";
+        document.getElementById('t10').innerHTML+=text;
+    }
+},18000);
